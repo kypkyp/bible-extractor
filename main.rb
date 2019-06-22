0e380e7c5e6807e43c95a1e8d49c93a3cf50ac23
kypkyp <kyp@kmc.gr.jp>
2019-06-22 14:55:29 +0900

    refactor

--- main.rb
+++ main.rb
@@ -1,10 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 
-url = 'https://live.bible.is/bible/PISWBT/MAT/1'
+url_base = 'https://live.bible.is/bible/PISWBT/'
 
-page = Nokogiri::HTML.parse(open(url))
+def single_page_extract(url)
+  page = Nokogiri::HTML.parse(open(url))
+  title = page.xpath("//meta[@name='twitter:title']/@content").to_s
 
-verses = page.xpath("//div[@class='chapter justify']/span/span")
+  verses = page.xpath("//div[@class='chapter justify']/span/span")
 
-puts verses.inner_text
+  File.open("output/#{title}.txt", 'w') do |f|
+    verses.each do |v|
+      f.puts(v.inner_text)
+    end
+  end
+end
+
+single_page_extract('https://live.bible.is/bible/PISWBT/MAT/1')
